e313a5d6d777b23f7fb23ace52d455dac948aabb
Alexandr Suhovoy <a.suhovoy@imi.de>
2017-08-23 16:02:51 +0300

    added header

---
+++ client/components/Common/Header.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+
+export default class Header extends Component {
+    render() {
+        return(
+            <div>
+                It's my awesome header!
+            </div>
+        )
+    }	
+}
