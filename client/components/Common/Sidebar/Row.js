d427c2d7c17f62105429269373f984dcb3991aee
Sergey Elizov <se@piogroup.net>
2017-09-15 18:29:15 +0300

    fix style of active items in the left menu

--- client/components/Common/Sidebar/Row.js
+++ client/components/Common/Sidebar/Row.js
@@ -3,19 +3,38 @@ import { NavLink } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import SecondLevelMenu from './SecondLevelMenu';
 
+/**
+ * Check link for active
+ *
+ * @param {string} match
+ * @param {object} location
+ * @returns {boolean}
+ */
 const checkLinkForActive = (match, location) => {
+    return !!~location.pathname.toLowerCase().indexOf(match.toLowerCase());
+};
+
+/**
+ * Check link for active
+ *
+ * @param {object} match
+ * @param {object} location
+ * @returns {boolean}
+ */
+const checkNavLinkForActive = (match, location) => {
     if (!match) {
         return false;
     }
-    return ~location.pathname.indexOf(match.path) ? true : false;
+
+    return checkLinkForActive(match.path, location);
 };
 
 export default function Row({className, name, childrens}) {
     return (
-        <li>
-            <NavLink activeClassName="active" to={`/${name.toLowerCase()}`} isActive={checkLinkForActive}>
-                <i className={className} aria-hidden="true"/>
-                <span className="nav-label">  {name} </span>
+        <li className={ checkLinkForActive(name, location) ? 'active' : '' }>
+            <NavLink activeClassName="active" to={ `/${name.toLowerCase()}` } isActive={ checkNavLinkForActive }>
+                <i className={ className } aria-hidden="true"/>
+                <span className="nav-label">{ name }</span>
                 {
                     childrens ?
                     <span className="fa arrow" />
@@ -24,7 +43,7 @@ export default function Row({className, name, childrens}) {
             </NavLink>
             {
                 childrens ?
-                <SecondLevelMenu childrens={childrens} /> : null
+                <SecondLevelMenu childrens={ childrens } /> : null
             }
         </li>
     )
