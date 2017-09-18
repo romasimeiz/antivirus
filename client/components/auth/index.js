b2734cb16c5456e304f222c9ec49aa9a1833a947
Sergey Elizov <se@piogroup.net>
2017-09-18 14:04:39 +0300

    #10545 Fix styles of the left menu

--- client/components/auth/index.js
+++ client/components/auth/index.js
@@ -4,14 +4,23 @@ import Sidebar from '../Common/Sidebar';
 import AppNotification from '../AppNotification'
 import './style.scss';
 
+const NAVBAR_STORAGE_KEY = 'navbar';
+const BODY_SMALL_WIDTH = 768;
+
 export default class Auth extends Component {
     constructor(props) {
         super(props);
-        this.state = {show: true};
+        this.state = {
+            show: (JSON.parse(window.localStorage.getItem(NAVBAR_STORAGE_KEY)) === false ? false : true),
+            width: window.innerWidth,
+            height: window.innerHeight
+        };
     }
 
     toggleSidebar = () => {
-        this.setState({show: !this.state.show});
+        let navbar = !this.state.show;
+        this.setState({show: navbar});
+        window.localStorage.setItem(NAVBAR_STORAGE_KEY, navbar)
     };
 
     componentWillMount() {
@@ -24,17 +33,34 @@ export default class Auth extends Component {
         }
     }
 
+    componentDidMount() {
+        this.updateWindowDimensions();
+        window.addEventListener('resize', () => this.updateWindowDimensions());
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('resize', () => this.updateWindowDimensions());
+    }
+
+    updateWindowDimensions() {
+        this.setState({ width: window.innerWidth, height: window.innerHeight });
+    }
+
     render() {
         const {handleLogout, auth, notification} = this.props;
+
+        document.body.classList.toggle('mini-navbar', !this.state.show);
+        document.body.classList.toggle('body-small', this.state.width <= BODY_SMALL_WIDTH);
+
         return (
-            <div className={"viewport " + (this.state.show ? '' : 'mini-navbar')}>
-                <AppNotification properties={notification} />
-                <Sidebar user={auth.user} fields={auth.sideBar}/>
+            <div className="viewport">
+                <AppNotification properties={ notification } />
+                <Sidebar user={ auth.user } fields={ auth.sideBar } show={ this.state.show } />
                 <div id="page-wrapper" className="gray-bg dashbard-1">
-                    <Header handleLogout={handleLogout} toggle={this.toggleSidebar}/>
+                    <Header handleLogout={ handleLogout } toggle={ this.toggleSidebar } />
                     <div className="body">
                         <h1>Auth</h1>
-                        {this.props.children}
+                        { this.props.children }
                     </div>
                 </div>
             </div>
