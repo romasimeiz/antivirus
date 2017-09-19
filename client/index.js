620c034b82c36b07e8d5591b9fbcae36c8fd9995
Roman Yephimov <ry@piogroup.net>
2017-09-19 17:54:55 +0300

    test

--- client/index.js
+++ client/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import {BreadcrumbsProvider} from 'react-breadcrumbs-dynamic';
 
 import './index.html';
 import './assets/css/index.css';
