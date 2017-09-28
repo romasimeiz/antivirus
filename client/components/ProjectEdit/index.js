a0673ac0c7fdc354c968f96ce17b1105fc4675c0
Sergey Elizov <se@piogroup.net>
2017-09-28 19:08:32 +0300

    fix

--- client/components/ProjectEdit/index.js
+++ client/components/ProjectEdit/index.js
@@ -1,78 +1,64 @@
 import React, { Component } from 'react';
 import { Field } from 'redux-form';
 import fieldDecorator from '../Common/field-decorator';
+import BoxWrapper from '../Common/BoxWrapper';
 
 import './project-form.scss';
 
 export default class ProjectEdit extends Component {
-    componentDidMount() {
+    componentWillMount() {
         this.props.match.params.projectId ? this.props.getProject(this.props.match.params.projectId) : false;
         this.props.getUsers({per_page: 999999});
-    }
-
-    componentWillMount() {
-        //this.props.match.params.projectId ? this.props.getProject(this.props.match.params.projectId) : false;
-        //this.props.getProject(this.props.match.params.projectId);
     }
 
     render() {
         const {isFetching, handleSubmit, users, initialValues, error } = this.props;
         const onSubmit = this.props.match.params.projectId ? this.props.onSubmitUpdate : this.props.onSubmitCreate;
         const usersArray = users.data ? users.data : null;
-        cl(onSubmit);
+
         return (
-            <div className="row">
-                <div className="loginscreen animated fadeInDown col-md-offse col-lg-offset-4 col-md-4">
-                    <div className="ibox float-e-margins">
-                        <div className="ibox-title">
-                            <h5>Title...</h5>
-                        </div>
-                        <div className="ibox-content">
-                            <form onSubmit={handleSubmit(onSubmit)} className="m-t">
-                                <Field name="user_id" component="select" className="form-control m-b">
-                                    <option value="">Select a user...</option>
-                                    {
-                                        usersArray ? usersArray.map(user =>
-                                        <option value={user.id} key={user.id}>
-                                            {user.name}
-                                        </option>
-                                    ) : false}
-                                </Field>
+            <BoxWrapper>
+                <form onSubmit={handleSubmit(onSubmit)} className="m-t">
+                    <Field name="user_id" component="select" className="form-control m-b">
+                        <option value="">Select a user...</option>
+                        {
+                            usersArray ? usersArray.map(user =>
+                            <option value={user.id} key={user.id}>
+                                {user.name}
+                            </option>
+                        ) : false}
+                    </Field>
 
-                                <Field label="Project name" name="name" className="form-control" component={fieldDecorator} type="text"
-                                       placeholder="Project Name" />
+                    <Field label="Project name" name="name" className="form-control" component={fieldDecorator} type="text"
+                           placeholder="Project Name" />
 
-                                <Field name="restrict_ip" className="form-control" component={fieldDecorator} type="text" label="Restrict IP"
-                                       placeholder="Restrict IP" />
+                    <Field name="restrict_ip" className="form-control" component={fieldDecorator} type="text" label="Restrict IP"
+                           placeholder="Restrict IP" />
 
-                                <Field name="script_url" className="form-control" component={fieldDecorator} type="text"
-                                       placeholder="Script URL" label="Script URL" />
+                    <Field name="script_url" className="form-control" component={fieldDecorator} type="text"
+                           placeholder="Script URL" label="Script URL" />
 
-                                <Field name="script_version" className="form-control" component={fieldDecorator} type="text"
-                                       placeholder="Script version" label="Script version"/>
+                    <Field name="script_version" className="form-control" component={fieldDecorator} type="text"
+                           placeholder="Script version" label="Script version"/>
 
-                                <Field name="host" className="form-control" component={fieldDecorator} type="text"
-                                       placeholder="Host" label="Host" />
+                    <Field name="host" className="form-control" component={fieldDecorator} type="text"
+                           placeholder="Host" label="Host" />
 
-                                <Field name="platform_name" className="form-control" component={fieldDecorator} type="text"
-                                       placeholder="Platform name" label="Platform name"/>
+                    <Field name="platform_name" className="form-control" component={fieldDecorator} type="text"
+                           placeholder="Platform name" label="Platform name"/>
 
-                                <Field name="platform_version" className="form-control" component={fieldDecorator} type="text"
-                                       placeholder="Platform version" label="Platform version"/>
+                    <Field name="platform_version" className="form-control" component={fieldDecorator} type="text"
+                           placeholder="Platform version" label="Platform version"/>
 
-                                <Field name="is_active" className="form-control" component={fieldDecorator}
-                                       style={{width: '10%'}} type="checkbox" label="Is active?" />
+                    <Field name="is_active" className="form-control" component={fieldDecorator}
+                           style={{width: '10%'}} type="checkbox" label="Is active?" />
 
-                                <button type="submit" className="btn btn-primary center-block m-b">
-                                    <i className="fa fa-save m-r-xs"> </i>
-                                    Save
-                                </button>
-                            </form>
-                        </div>
-                    </div>
-                </div>
-                {cl(initialValues)}
-            </div>
+                    <button type="submit" className="btn btn-primary center-block m-b">
+                        <i className="fa fa-save m-r-xs"> </i>
+                        Save
+                    </button>
+                </form>
+            </BoxWrapper>
         );
     }
 }
